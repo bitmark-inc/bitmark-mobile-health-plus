328ab8f14f4d594910c53998584ac86c52581b89
bachlx <bachlx@bitmark.com>
2018-02-09 10:04:27 +0700

    revert debug code

--- client-app/src/components/onboarding/sign-in/sign-in.component.js
+++ client-app/src/components/onboarding/sign-in/sign-in.component.js
@@ -24,8 +24,8 @@ let PreCheckResults = {
   error: 'RETRY'
 };
 
-let testWords = ['absurd', 'defy', 'group', 'forward', 'curious', 'erase', 'dwarf', 'seminar', 'stairs',
-  'abandon', 'earn', 'civil', 'amount', 'junk', 'stove', 'army', 'install', 'goat', 'tunnel', 'tunnel', 'eagle', 'sell', 'ridge', 'guess'];
+// let testWords = ['absurd', 'defy', 'group', 'forward', 'curious', 'erase', 'dwarf', 'seminar', 'stairs',
+//   'abandon', 'earn', 'civil', 'amount', 'junk', 'stove', 'army', 'install', 'goat', 'tunnel', 'tunnel', 'eagle', 'sell', 'ridge', 'guess'];
 
 export class SignInComponent extends React.Component {
 
@@ -46,14 +46,14 @@ export class SignInComponent extends React.Component {
       if (index < 12) {
         smallerList.push({
           key: index,
-          word: testWords[index],
-          // word: '',
+          // word: testWords[index],
+          word: '',
         });
       } else {
         biggerList.push({
           key: index,
-          word: testWords[index],
-          // word: '',
+          // word: testWords[index],
+          word: '',
         });
       }
     }
@@ -67,7 +67,7 @@ export class SignInComponent extends React.Component {
       dataSource: dictionary24Words,
       keyBoardHeight: 0,
     };
-    setTimeout(this.checkStatusInputing, 200);
+    // setTimeout(this.checkStatusInputing, 200);
   }
 
   onChangeText(index, text) {
