3c89c3f3853bfafa57197833d76ed4211ba87e00
Anh Nguyen <anhnguyen@bitmark.com>
2018-02-26 14:42:41 +0700

    Update the position of parameters of requestSession function

--- client-app/ios/Bitmark/RNModules/BitmarkSDK/BitmarkSDK.swift
+++ client-app/ios/Bitmark/RNModules/BitmarkSDK/BitmarkSDK.swift
@@ -219,7 +219,7 @@ class BitmarkSDK: NSObject {
   }
   
   @objc(requestSession:::)
-  func requestSession(_ reason: String, _ network: String, _ callback: @escaping RCTResponseSenderBlock) {
+  func requestSession(_ network: String, _ reason: String, _ callback: @escaping RCTResponseSenderBlock) {
     do {
       guard let sessionId = try AccountSession.shared.requestSession(reason: reason, network: network) else {
         callback([false])
