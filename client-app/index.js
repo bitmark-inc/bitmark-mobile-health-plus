c4c24f59f17a9203151ef338d192f16736c7bcc9
bachlx <bachlx@bitmark.com>
2018-04-09 12:16:04 +0700

    update code push config

--- client-app/index.js
+++ client-app/index.js
@@ -2,6 +2,14 @@ import codePush from "react-native-code-push";
 import { AppRegistry } from 'react-native';
 import App from './App';
 
-let codePushOptions = { checkFrequency: codePush.CheckFrequency.ON_APP_RESUME };
+let codePushOptions = {
+  updateDialog: true,
+  // updateDialog: {
+  //   title: 'New version is avaiable',
+  //   mandatoryUpdateMessage: 'You have to install new version to use app!',
+  //   mandatoryContinueButtonLabel: "Install"
+  // },
+  installMode: codePush.InstallMode.IMMEDIATE
+};
 
 AppRegistry.registerComponent('Bitmark', () => codePush(codePushOptions)(App));
