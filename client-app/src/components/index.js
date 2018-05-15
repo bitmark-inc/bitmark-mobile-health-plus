bc96462a911d794a9057747f8bfc8cb8e1634707
bachlx <bachlx@bitmark.com>
2018-05-15 10:22:23 +0700

    revert code event emiter

--- client-app/src/components/index.js
+++ client-app/src/components/index.js
@@ -92,6 +92,9 @@ class MainComponent extends Component {
     });
   }
   componentWillUnmount() {
+    EventEmiterService.remove(EventEmiterService.events.APP_PROCESSING, this.handerProcessingEvent);
+    EventEmiterService.remove(EventEmiterService.events.APP_SUBMITTING, this.handerSumittinggEvent);
+    EventEmiterService.remove(EventEmiterService.events.APP_PROCESS_ERROR, this.handerProcessErrorEvent);
     Linking.addEventListener('url', this.handleDeppLink);
     AppState.removeEventListener('change', this.handleAppStateChange);
     NetInfo.isConnected.removeEventListener('connectionChange', this.handleNetworkChange);
