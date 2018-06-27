2dc1dd7366fcb6bd79b3d66741cc1c1feaa94356
bachlx <bachlx@bitmark.com>
2018-06-27 16:29:15 +0700

    mark need reset local data

--- client-app/src/configs/index.js
+++ client-app/src/configs/index.js
@@ -29,7 +29,7 @@ let config = {
   web_app_server_url: 'http://192.168.0.109:8900',
   preview_asset_url: 'https://preview.test.bitmarkaccountassets.com',
   bitmark_web_site: 'https://website.test.bitmark.com',
-  needResetLocalData: 1530063408553,
+  needResetLocalData: 1530091731834,
 };
 
 if (config.network === NETWORKS.testnet) {
