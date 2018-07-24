fb22088e87c24536872f6af714ade3423905a57b
Anh Nguyen <anhnguyen@bitmark.com>
2018-07-24 13:43:53 +0700

    Correct handling timeoutContext

--- mobile-server/server/server.go
+++ mobile-server/server/server.go
@@ -3,9 +3,11 @@ package server
 import (
 	"context"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx"
+	log "github.com/sirupsen/logrus"
 
 	"github.com/bitmark-inc/mobile-app/mobile-server/logmodule"
 	"github.com/bitmark-inc/mobile-app/mobile-server/store/bitmarkstore"
@@ -88,11 +90,31 @@ func (s *Server) HealthCheck(c *gin.Context) {
 	}
 	defer conn.Close()
 
-	if err = conn.Ping(c); err != nil {
-		c.AbortWithStatusJSON(500, gin.H{
-			"error": "Failed to connect to db.",
-		})
-		return
+	ctx, cancel := context.WithTimeout(c, 2*time.Second)
+	defer cancel()
+
+	errChan := make(chan error)
+
+	go func() {
+		errChan <- conn.Ping(ctx)
+	}()
+
+	select {
+	case <-ctx.Done():
+		if ctx.Err() != nil {
+			log.Error(ctx.Err())
+			c.AbortWithStatusJSON(500, gin.H{
+				"error": "Timeout connect to db.",
+			})
+			return
+		}
+	case err := <-errChan:
+		if err != nil {
+			c.AbortWithStatusJSON(500, gin.H{
+				"error": "Failed to connect to db.",
+			})
+			return
+		}
 	}
 
 	c.JSON(200, gin.H{
