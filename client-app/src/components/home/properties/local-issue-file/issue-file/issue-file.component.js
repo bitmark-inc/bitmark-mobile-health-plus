7fa8c90cb463a009c1b0be3d910b98125034fb1b
bachlx <bachlx@bitmark.com>
2018-07-25 15:53:51 +0700

    update event name and emit event for issuance

--- client-app/src/components/home/properties/local-issue-file/issue-file/issue-file.component.js
+++ client-app/src/components/home/properties/local-issue-file/issue-file/issue-file.component.js
@@ -80,15 +80,15 @@ export class LocalIssueFileComponent extends React.Component {
   // ==========================================================================================
   onIssueFile() {
     console.log('assetAccessibility :', this.state.assetAccessibility);
-    // TODO
-    CommonModel.doTrackEvent({
-      event_name: 'app_user_done_issuance',
-      account_number: DataProcessor.getUserInformation().bitmarkAccountNumber,
-    });
+
     AppProcessor.doIssueFile(this.state.filePath, this.state.assetName, this.state.metadataList, parseInt(this.state.quantity), this.state.assetAccessibility === 'public', {
       indicator: true, title: '', message: 'Sending your transaction to the Bitmark network...'
     }).then((data) => {
       if (data) {
+        CommonModel.doTrackEvent({
+          event_name: 'app_user_done_issuance',
+          account_number: DataProcessor.getUserInformation().bitmarkAccountNumber,
+        });
         // Remove temp asset file
         FileUtil.removeSafe(this.state.filePath);
         Alert.alert('Success!', 'Your property rights have been registered.', [{
