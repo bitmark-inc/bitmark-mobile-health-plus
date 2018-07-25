d885bc5bd4a4180b1ab74eca4d152ed2d92e1af8
bachlx <bachlx@bitmark.com>
2018-07-25 15:58:01 +0700

    update check data when emit event

--- client-app/src/processors/data-processor.js
+++ client-app/src/processors/data-processor.js
@@ -46,7 +46,7 @@ const doCheckNewIftttInformation = async (iftttInformation, isLoadingAllUserData
       // TODO
       await CommonModel.doTrackEvent({
         event_name: 'app_user_connected_ifttt',
-        account_number: DataProcessor.getUserInformation().bitmarkAccountNumber,
+        account_number: userInformation ? userInformation.bitmarkAccountNumber : null,
       });
     }
 
@@ -358,7 +358,7 @@ const configNotification = () => {
       }
       await CommonModel.doTrackEvent({
         event_name: 'app_user_click_notification',
-        account_number: userInformation.bitmarkAccountNumber,
+        account_number: userInformation ? userInformation.bitmarkAccountNumber : null,
       });
       setTimeout(async () => {
         EventEmitterService.emit(EventEmitterService.events.APP_RECEIVED_NOTIFICATION, notificationData.data);
@@ -465,7 +465,7 @@ const doOpenApp = async () => {
     await CommonModel.doSetLocalData(CommonModel.KEYS.APP_INFORMATION, appInfo);
     await CommonModel.doTrackEvent({
       event_name: 'app_download',
-      account_number: userInformation.bitmarkAccountNumber,
+      account_number: userInformation ? userInformation.bitmarkAccountNumber : null,
     });
   }
   if (!appInfo.trackEvents || !appInfo.trackEvents.app_download_location) {
@@ -482,7 +482,7 @@ const doOpenApp = async () => {
       await CommonModel.doSetLocalData(CommonModel.KEYS.APP_INFORMATION, appInfo);
       await CommonModel.doTrackEvent({
         event_name: 'app_download_location',
-        account_number: userInformation.bitmarkAccountNumber,
+        account_number: userInformation ? userInformation.bitmarkAccountNumber : null,
         position: JSON.stringify(locationDetail.position),
         city: locationDetail.locality,
         country: locationDetail.country,
