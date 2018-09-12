918b5a8fdec99d842907a8b2b76c884ebb6339dd
bachlx <bachlx@bitmark.com>
2018-09-12 14:32:54 +0700

    update text

--- BitmarkHealthPlus/src/components/user/granting-access.component.js
+++ BitmarkHealthPlus/src/components/user/granting-access.component.js
@@ -50,7 +50,7 @@ export class GrantingAccessComponent extends Component {
         <a href="healthplus://granting-access/${this.state.token}">healthplus://granting-access/${this.state.token}</a>
         <br/>
         <br/>
-        Please install <a href="${config.appLink}">Bitmark Health+</a> app and sign in before confirm the authorization.`,
+        Please install <a href="${config.appLink}">Bitmark Health+</a> app and sign in before clicking the link.`,
       isHTML: true,
     }, (error) => {
       if (error) {
