7c46f87c60b2ac1536d253e87c9e06f24887b38e
Anh Nguyen <anhnguyen@bitmark.com>
2018-11-05 15:44:13 +0700

    Fix typo

--- ios/Bitmark/RNModules/iCloudSync/iCloudSync.m
+++ ios/Bitmark/RNModules/iCloudSync/iCloudSync.m
@@ -13,7 +13,7 @@
 
 RCT_EXPORT_MODULE();
 
-RCT_EXPORT_METHOD(uploadToCloudFile:(NSString *)folder:(RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD(uploadToCloud:(NSString *)folder:(RCTResponseSenderBlock)callback)
 {
   NSString *documentsDirectory = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)[0];
   NSString *localDocument = [documentsDirectory stringByAppendingPathComponent:folder];
