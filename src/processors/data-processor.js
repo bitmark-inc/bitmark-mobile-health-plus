f8f9a7a3df8400a0782c9efc9540a826a638e27b
bachlx <bachlx@bitmark.com>
2018-11-28 09:00:45 +0700

    update check local storage migration

--- src/processors/data-processor.js
+++ src/processors/data-processor.js
@@ -52,9 +52,6 @@ const mapModalDisplayKeyIndex = {
   update_alpha_app: 5,
 };
 let codePushUpdated = null;
-// let isDisplayingEmailRecord = false;
-// let isMigratingFileToLocalStorage = false;
-let didMigrationFileToLocalStorage = false;
 let mountedRouter = null;
 
 const isHealthDataBitmark = (asset) => {
@@ -664,18 +661,14 @@ const doOpenApp = async (justCreatedBitmarkAccount) => {
 
     if (justCreatedBitmarkAccount) {
       await AccountModel.doMarkMigration(jwt);
-      didMigrationFileToLocalStorage = true;
+      appInfo.didMigrationFileToLocalStorage = true;
       appInfo.displayedWhatNewInformation = DeviceInfo.getVersion();
       await CommonModel.doSetLocalData(CommonModel.KEYS.APP_INFORMATION, appInfo);
-    } else {
-      didMigrationFileToLocalStorage = await AccountModel.doCheckMigration(jwt);
+    } else if (!appInfo.didMigrationFileToLocalStorage) {
+      let didMigrationFileToLocalStorage = await AccountModel.doCheckMigration(jwt);
       if (!didMigrationFileToLocalStorage && !isDisplayingModal(mapModalDisplayKeyIndex.local_storage_migration)) {
         updateModal(mapModalDisplayKeyIndex.local_storage_migration, true);
       }
-      // if (!didMigrationFileToLocalStorage && !isMigratingFileToLocalStorage) {
-      //   isMigratingFileToLocalStorage = true;
-      //   EventEmitterService.emit(EventEmitterService.events.APP_MIGRATION_FILE_LOCAL_STORAGE);
-      // }
       if (!appInfo.displayedWhatNewInformation || compareVersion(appInfo.displayedWhatNewInformation, DeviceInfo.getVersion(), 2) < 0) {
         updateModal(mapModalDisplayKeyIndex.what_new, true);
       }
@@ -1181,7 +1174,9 @@ const doMigrateFilesToLocalStorage = async () => {
 
   EventEmitterService.emit(EventEmitterService.events.APP_MIGRATION_FILE_LOCAL_STORAGE_PERCENT, 100);
   await AccountModel.doMarkMigration(jwt);
-  didMigrationFileToLocalStorage = true;
+  let appInfo = await doGetAppInformation();
+  appInfo.didMigrationFileToLocalStorage = true;
+  await CommonModel.doSetLocalData(CommonModel.KEYS.APP_INFORMATION, appInfo);
 };
 
 const detectLocalAssetFilePath = async (assetId) => {
