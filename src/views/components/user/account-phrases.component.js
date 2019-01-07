f1a7a853e5721b76a41f408607489f6002a28afa
bachlx <bachlx@bitmark.com>
2019-01-07 17:39:39 +0700

    update layout

--- src/views/components/user/account-phrases.component.js
+++ src/views/components/user/account-phrases.component.js
@@ -585,7 +585,7 @@ export class AccountPhraseComponent extends Component {
                                         borderWidth: 1,
                                         borderColor: this.state.testingResult === false ? '#FF4444' : '#0060F2',
                                         color: this.state.testingResult === false ? '#FF4444' : '#0060F2',
-                                      }, item.characters ? {} : { marginLeft: 0, height: 20, paddingTop: 4, paddingBottom: 4, paddingRight: 0, }]}>
+                                      }, item.characters ? {} : { marginLeft: 0, height: 20, paddingRight: 1, paddingLeft: 1 }]}>
                                         <TextInput
                                           style={[styles.recoveryPhraseInputWord, { color: this.state.testingResult === false ? '#FF4444' : '#0060F2', }]}
                                           ref={(r) => { this.inputtedRefs[item.key] = r; }}
@@ -631,7 +631,7 @@ export class AccountPhraseComponent extends Component {
                                         backgroundColor: 'white',
                                         borderWidth: 1,
                                         borderColor: this.state.testingResult === false ? '#FF4444' : '#0060F2',
-                                      }, item.characters ? {} : { marginLeft: 0, height: 20, paddingTop: 4, paddingBottom: 4, paddingRight: 0, }]}>
+                                      }, item.characters ? {} : { marginLeft: 0, height: 20, paddingRight: 1, paddingLeft: 1 }]}>
                                         <TextInput
                                           style={[styles.recoveryPhraseInputWord, { color: this.state.testingResult === false ? '#FF4444' : '#0060F2', }]}
                                           ref={(r) => { this.inputtedRefs[item.key] = r; }}
@@ -880,6 +880,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFFFFF',
     fontFamily: config.localization.startsWith('vi') ? 'Avenir Next' : 'Andale Mono',
     fontSize: 13,
+    paddingLeft: 2, paddingRight: 2
   },
   infoLinkTextContainer: {
     flexDirection: 'row',
