740057313880a58e86906fd9054ed92d8fb35bde
Anh Nguyen <anhnguyen@bitmark.com>
2019-02-15 12:03:06 +0700

    Update BitmarkSDK

--- ios/Pods/BitmarkSDK/BitmarkSDK/Utils/Migration.swift
+++ ios/Pods/BitmarkSDK/BitmarkSDK/Utils/Migration.swift
@@ -9,22 +9,75 @@
 import Foundation
 
 public struct Migration {
-    public static func migrate(recoverPhrase: [String], language: RecoveryLanguage) throws -> (Account, [String]) {
-        let accountToMigrate = try Account(recoverPhrase: recoverPhrase, language: language)
-        if accountToMigrate.seed.version != .v1 {
-            throw("Only support migration from bitmark account version 1")
-        }
-        
+//    public static func migrate(recoverPhrase: [String], language: RecoveryLanguage) throws -> (Account, [String]) {
+//        let accountToMigrate = try Account(recoverPhrase: recoverPhrase, language: language)
+//        if accountToMigrate.seed.version != .v1 {
+//            throw("Only support migration from bitmark account version 1")
+//        }
+//
+//        // Query for current owning bitmarks
+//        var lastOffset: Int64? = nil
+//        var owningBitmarks = [Bitmark]()
+//        var shouldContinue = true
+//        while shouldContinue {
+//            var bitmarksQuery = Bitmark.newBitmarkQueryParams()
+//                .owned(by: accountToMigrate.address)
+//                .loadAsset(true)
+//                .to(direction: .earlier)
+//                .includePending(true)
+//
+//            if let lastOffset = lastOffset {
+//                bitmarksQuery = bitmarksQuery.at(lastOffset)
+//            }
+//
+//            let (bitmarks, _) = try Bitmark.list(params: bitmarksQuery)
+//
+//            if bitmarks == nil || bitmarks?.count == 0 {
+//                break
+//            }
+//
+//            if bitmarks!.count < 100 {
+//                shouldContinue = false
+//            }
+//
+//            lastOffset = bitmarks!.last!.offset
+//
+//            owningBitmarks += bitmarks!
+//        }
+//
+//        // Group bitmarks with their asset_id
+//        let assetBitmarkCountMap = Dictionary(grouping: owningBitmarks,
+//                                         by: { $0.asset_id })
+//            .mapValues( { $0.count } )
+//
+//        // Create a new bitmark account v2
+//        let newAccount = try Account(version: .v2)
+//
+//        // Reissue new bitmarks with new account
+//        var bitmarkIds = [String]()
+//
+//        for (assetId, count) in assetBitmarkCountMap {
+//            var issuanceParam = try Bitmark.newIssuanceParams(assetID: assetId, owner: newAccount.accountNumber, quantity: count)
+//            try issuanceParam.sign(accountToMigrate)
+//            let result = try Bitmark.issue(issuanceParam)
+//            bitmarkIds += result
+//        }
+//
+//        return (newAccount, bitmarkIds)
+//    }
+    
+    // Query and transfer all of current confirmed properties
+    public static func rekey(from accountFrom: Account, to accountTo: Account) throws {
         // Query for current owning bitmarks
         var lastOffset: Int64? = nil
-        var owningBitmarks = [Bitmark]()
         var shouldContinue = true
+        
         while shouldContinue {
             var bitmarksQuery = Bitmark.newBitmarkQueryParams()
-                .owned(by: accountToMigrate.address)
+                .owned(by: accountFrom.address)
                 .loadAsset(true)
                 .to(direction: .earlier)
-                .includePending(true)
+                .includePending(false)
             
             if let lastOffset = lastOffset {
                 bitmarksQuery = bitmarksQuery.at(lastOffset)
@@ -42,27 +95,22 @@ public struct Migration {
             
             lastOffset = bitmarks!.last!.offset
             
-            owningBitmarks += bitmarks!
+            for bitmark in bitmarks! {
+                var transferRequest = TransferRequest()
+                transferRequest.set(fromOwner: accountFrom.accountNumber)
+                try transferRequest.set(to: accountTo.accountNumber)
+                transferRequest.set(fromTx: bitmark.id)
+                transferRequest.set(requireCountersignature: true)
+                try transferRequest.sign(accountFrom)
+                
+                var counterTransfer = CountersignedTransferRequest(link: bitmark.id,
+                                                                   owner: transferRequest.owner!,
+                                                                   signature: transferRequest.signature!.hexEncodedString)
+                try counterTransfer.sign(accountTo)
+                
+                let api = API()
+                _ = try api.transfer(withCounterTransfer: counterTransfer)
+            }
         }
-        
-        // Group bitmarks with their asset_id
-        let assetBitmarkCountMap = Dictionary(grouping: owningBitmarks,
-                                         by: { $0.asset_id })
-            .mapValues( { $0.count } )
-        
-        // Create a new bitmark account v2
-        let newAccount = try Account(version: .v2)
-        
-        // Reissue new bitmarks with new account
-        var bitmarkIds = [String]()
-        
-        for (assetId, count) in assetBitmarkCountMap {
-            var issuanceParam = try Bitmark.newIssuanceParams(assetID: assetId, owner: newAccount.accountNumber, quantity: count)
-            try issuanceParam.sign(accountToMigrate)
-            let result = try Bitmark.issue(issuanceParam)
-            bitmarkIds += result
-        }
-        
-        return (newAccount, bitmarkIds)
     }
 }
