f1f91a8e87f4e881bfb16c09621ffbd358dc972e
bachlx <bachlx@bitmark.com>
2019-02-25 17:24:51 +0700

    code same as registry

--- ios/Bitmark/AppDelegate.m
+++ ios/Bitmark/AppDelegate.m
@@ -12,9 +12,7 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
-#ifndef DEBUG
 #import <React/RNSentry.h> // This is used for versions of react >= 0.40
-#endif
 #import <React/RCTLinkingManager.h>
 #import <React/RCTPushNotificationManager.h>
 #import <React/RCTLog.h>
